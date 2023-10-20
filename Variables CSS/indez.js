bc74432b1a5404aade922547a5aa3f8ea4fc7e24
DavidMoya18 <david1998moya18@gmail.com>
2023-10-20 18:46:58 -0500

    first commit

---
+++ Variables CSS/indez.js
@@ -0,0 +1,54 @@
+//Primer ejercisio
+let miNombre = "David";
+nombre = "Mi nombre es " + miNombre;
+console.log(nombre);
+
+//Segundo ejercisio
+let age;
+age = 24;
+edad = "Mi edad es " + age + " años";
+console.log(edad);
+
+//Tercer ejercisio
+let precioProducto = "$48.99";
+console.log(precioProducto);
+
+//Cuarto ejercisio
+let numero1 = 18;
+let numero2 = 25;
+
+suma= numero1 + numero2;
+
+console.log(suma);
+
+//Quinto ejercisio
+let temperaturaCelsius = 25;
+
+fahrenheit = (temperaturaCelsius * 9/5) + 32;
+console.log(fahrenheit +"°c");
+
+//Sexto ejercisio
+let longitudLado ="15";
+
+areaCuadrado = longitudLado * longitudLado;
+
+console.log(areaCuadrado);
+
+//Septimo ejercisio
+let cadena1 = "Hola";
+let cadena2 = "Mundo";
+
+concatenar = cadena1 + " " + cadena2 ;
+console.log(concatenar);
+
+
+
+
+
+
+
+
+
+
+
+ 
